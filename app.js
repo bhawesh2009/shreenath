33d3e5b78fb20993983d0b57fe1a197b839ebbb9
bhawesh2009 <bpaliwal980@gmail.com>
2021-03-13 13:36:56 +0530

    main

--- app.js
+++ app.js
@@ -47,10 +47,10 @@ app.get("/api/config/paypal", (req, res) =>
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/public")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {

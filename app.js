6774648774ae5967abf23ea402d281a0a03fa64e
bhawesh2009 <bpaliwal980@gmail.com>
2021-03-13 20:57:37 +0530

    main

--- app.js
+++ app.js
@@ -46,8 +46,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
+    res.sendFile(path.resolve("app/client/build/index.html")));
 } else {
   app.get("/", (req, res) => {
     res.send("API is running....");

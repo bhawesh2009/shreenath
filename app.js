e7cdb7c9ff109182999ecf845a8f580da1da9e1b
bhawesh2009 <bpaliwal1980@gmail.com>
2021-03-14 18:47:32 +0530

    main

--- app.js
+++ app.js
@@ -55,8 +55,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.use(unknownEndpoints)
-app.use(errorHandler)
+app.use(unknownEndpoints);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 

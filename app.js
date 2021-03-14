6a2f78dad36975e5449666d7d3fa96f903af68f2
bhawesh2009 <bpaliwal1980@gmail.com>
2021-03-14 13:00:47 +0530

    main

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.get("/api/config/paypal", (req, res) =>
 );
 
 if (process.env.NODE_ENV === "production") {
-  app.use('/', express.static(path.join(__dirname, '/client/build')));
+  app.use(express.static(__dirname + '/client/build'));
   app.get('*', (req, res) =>{
     res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
   });

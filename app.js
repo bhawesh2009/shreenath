b940159e59f5868461aa14032354e5c2edfb678f
bhawesh2009 <bpaliwal1980@gmail.com>
2021-03-15 07:53:25 +0530

    main

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.get("/api/config/paypal", (req, res) =>
 );
 
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
 
 
 if (process.env.NODE_ENV === 'PRODUCTION') {
